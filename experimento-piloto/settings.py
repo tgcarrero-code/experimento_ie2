49baf091f233931fb6cbd5d236d0d730cdaec51f
tgcarrero-code <tg.carreror@alum.up.edu.pe>
2026-08-31 13:11:08 -0500

    Add files via upload

---
+++ experimento-piloto/settings.py
@@ -0,0 +1,21 @@
+
+SESSION_CONFIGS = [
+    dict(
+        name='justicia_bienestar',
+        display_name='Justicia y Bienestar',
+        app_sequence=['justicia_bienestar'],
+        num_demo_participants=20,
+    ),
+]
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1,
+    participation_fee=7.50,
+)
+
+LANGUAGE_CODE = 'es'
+REAL_WORLD_CURRENCY_CODE = 'PEN'
+USE_POINTS = False
+SECRET_KEY = 'justicia_bienestar_2026_secret_key'
+ADMIN_PASSWORD = 'admin123'
+DEBUG = True
