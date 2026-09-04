892fe738812caffe62563d0fa2e9ec38ab1c4ede
tgcarrero-code <tg.carreror@alum.up.edu.pe>
2026-09-04 00:06:25 -0500

    Update settings.py

--- experimento-piloto/settings.py
+++ experimento-piloto/settings.py
@@ -1,4 +1,3 @@
-
 SESSION_CONFIGS = [
     dict(
         name='justicia_bienestar',
@@ -17,5 +16,6 @@ LANGUAGE_CODE = 'es'
 REAL_WORLD_CURRENCY_CODE = 'PEN'
 USE_POINTS = False
 SECRET_KEY = 'justicia_bienestar_2026_secret_key'
+ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = 'admin123'
 DEBUG = True
